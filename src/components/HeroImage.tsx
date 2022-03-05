1915339578338b78b0c1bb62ee6ca94157f2e5ee
Tiago <tiago@Tiagos-MacBook-Air.local>
2022-03-04 22:09:07 -0300

    put img inside a section

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,9 +1,13 @@
+import '../styles/HeroImage.css';
+
 type HeroImageProps = {
   imageSrc: string;
 };
 
 const HeroImage = ({ imageSrc }: HeroImageProps) => (
-  <img src={imageSrc} alt='A woman with orange shirt' />
+  <section className='hero'>
+    <img className='hero__img' src={imageSrc} alt='A woman with orange shirt' />
+  </section>
 );
 
 export default HeroImage;
